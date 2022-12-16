b4f0a808ca9d039a7fcc25e2d9dc6e510f05402b
Itzamna Huerta <itzamnahuerta@itzamnas-mbp.mynetworksettings.com>
2022-12-16 17:30:04 -0500

    rendered 2 regions - 1 column attribute

--- final_project/src/components/Viz.jsx
+++ final_project/src/components/Viz.jsx
@@ -44,7 +44,7 @@ export default class Viz extends Component {
     // console.log('the data',data)
     
     // SETTING UP SVG FOR D3
-    const svg = d3.select('svg')
+    // const svg = d3.select('svg')
 
 
     // COUNT NUMBER FOR REBEL GROUPS THAT MEET EACH ATTRIBUTE
@@ -55,6 +55,8 @@ export default class Viz extends Component {
     // - - - - - - - - - - - - 
     // REGIONS
     // - - - - - - - - - - - - 
+
+
 
     // 1. Mapping through data to only render latin america regions
     window.latinAmericaData = data.map((key,value) => {
@@ -72,6 +74,56 @@ export default class Viz extends Component {
         return key;
       }
     })
+
+    // 3. Mapping through data to only render Mid Eastern Africa regions
+    window.midEastnAfricaData = data.map((key,value) => {
+      if(key['mideastnafrica'] == 1){
+        // console.log("Mid Eastern Africa:",key) 
+        return key;
+      }
+    })
+
+    // 4. Mapping through data to only render West regions
+    window.westData = data.map((key,value) => {
+      if(key['west'] == 1){
+        // console.log("West:",key) 
+        return key;
+      }
+    })
+
+    // 5. Mapping through data to only render Central Eurasia regions
+    window.centralEurasiaData = data.map((key,value) => {
+      if(key['centraleurasia'] == 1){
+        // console.log("Central Eurasia:",key) 
+        return key;
+      }
+    })
+
+    // 6. Mapping through data to only render South Asia regions
+    window.southAsiaData = data.map((key,value) => {
+      if(key['southasia'] == 1){
+        // console.log("South Asia:",key) 
+        return key;
+      }
+    })  
+
+    // 7. Mapping through data to only render South East Asia regions
+    window.southEastAsiaData = data.map((key,value) => {
+      if(key['seasia'] == 1){
+        // console.log("South East Asia:",key) 
+        return key;
+      }
+    })
+
+    // 8. Mapping through data to only render East Asia regions
+    window.eastAsiaData = data.map((key,value) => {
+      if(key['eastasia'] == 1){
+        // console.log("East Asia:",key) 
+        return key;
+      }
+    })
+
+
 
     // __________________________________________________________
     // - - - - - - - - - - - - - - - - -
@@ -113,25 +165,30 @@ export default class Viz extends Component {
     // FUNCTION TO DISPLAY FOUGHT IN CIVIL WAR TO THE DOM
     let foughtCivilWar = () => {
       // LATIN AMERICA
-      // const latinAmericaSvg = svg.selectAll('#c2-r2')
-      // .data(foughtinCivilWar(window.latinAmericaData)) 
-      // .enter()
-      // .append('g')
-      // .attr("transform", (d,i) => {
-      //   const x = (i % perRow + 2) * size;
-      //   const y = (Math.floor(i/perRow) +.60) * sizeY;
-      //   return "translate(" + [x,y] + ")"
-      // });
+      const latinAmericaSvg = 
+      d3.select('#c2-r2')
+      .selectAll('#c2-r2')
+      .data(foughtinCivilWar(window.latinAmericaData)) 
+      .enter()
+      .append('g')
+      .attr("transform", (d,i) => {
+        const x = (i % perRow + 2) * size;
+        const y = (Math.floor(i/perRow) +.60) * sizeY;
+        return "translate(" + [x,y] + ")"
+      });
 
-      // latinAmericaSvg.append('path')
-      // .attr("d", rebel_icon)
-      // .attr("width", 600)
-      // .attr("height", 600)
-      // .attr("fill", "red")
+      latinAmericaSvg.append('path')
+      .attr("d", rebel_icon)
+      .attr("width", 600)
+      .attr("height", 600)
+      .attr("fill", "red")
 
       // AFRICA
 
-      const africaSvg = svg.selectAll('#c2-r3')
+      const africaSvg = 
+      d3.select('#c2-r3')
+      .selectAll('#c2-r3')
+      .attr("id","c2-r3")
       .data(foughtinCivilWar(window.africaData)) 
       .enter()
       .append('g')
@@ -157,7 +214,7 @@ export default class Viz extends Component {
 
     d3.select('#c1-r1')
     .on('click',function(){
-      console.log("fought in civil war btn clicked! ")
+      console.log("fought in civil war btn clicked!")
       foughtCivilWar()
     })
 
@@ -197,6 +254,13 @@ export default class Viz extends Component {
         {/* REGIONS */}
         <h4  id="c1-r2"> LATIN AMERICA</h4>
         <h4  id="c1-r3"> AFRICA </h4>
+        <h4  id="c1-r4"> MID EASTERN AFRICA </h4>
+        <h4  id="c1-r5"> WEST </h4>
+        <h4  id="c1-r6"> CENTRAL EURASIA </h4>
+        <h4  id="c1-r7"> SOUTH ASIA </h4>
+        <h4  id="c1-r8"> SOUTH EAST ASIA </h4>
+        <h4  id="c1-r9"> EAST ASIA </h4>
+
         {/* ATTRIBUTES */}
         <h4  id="c1-r1" > FOUGHT IN CIVIL WAR</h4>
         <svg 
